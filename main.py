6c64a313cffbc86d5e25b5d32f4b7355469c3963
saivarun1103 <varun.11n03@gmail.com>
2026-03-24 22:19:18 +0530

    day2 updates

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from src.utils.db import Base, engine
 from src.tasks.router import task_routes
+from src.user.router import user_routes
 
 Base.metadata.create_all(engine)
 
 app = FastAPI(title="This is a task management app")
 app.include_router(task_routes)
-
+app.include_router(user_routes)
---
+++ src/user/controller.py
@@ -0,0 +1,66 @@
+from fastapi import HTTPException, status, Request
+from src.user.dtos import UserSchema, LoginSchema
+from sqlalchemy.orm import Session
+from src.user.models import UserModel
+from pwdlib import PasswordHash
+from src.utils.settings import settings
+from datetime import datetime, timedelta
+import jwt
+
+password_hash = PasswordHash.recommended()
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+def register(body:UserSchema, db:Session):
+    print(body)
+    ## 1. Username Validation
+    is_user  = db.query(UserModel).filter(UserModel.username == body.username).first()
+    if is_user:
+        raise HTTPException(400, detail="Username already exist..")
+    ## 2. Email Validtation
+    is_user = db.query(UserModel).filter(UserModel.email == body.email).first()
+    if is_user:
+        raise HTTPException(400, detail="Email already registered..")
+    
+    hash_password = get_password_hash(body.password)
+
+    new_user = UserModel(
+        name = body.name,
+        username = body.username,
+        hash_password = hash_password,
+        email = body.email
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
+
+def login_user(body: LoginSchema, db:Session):
+    user  = db.query(UserModel).filter(UserModel.username == body.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username..")
+    
+    if not verify_password(body.password, user.hash_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid password..")
+
+    exp_time = datetime.now() + timedelta(minutes=settings.EXP_TIME)
+
+    token = jwt.encode({"_id":user.id, "exp":exp_time}, settings.SECRET_KEY, settings.ALGORITHM)
+
+    return {"token":token}
+
+
+def is_authenticated(request:Request, db:Session):
+    token = request.headers.get("authorization")
+    token = token.split(" ")[-1]
+    data = jwt.decode(token, settings.SECRET_KEY, settings.ALGORITHM)
+    print(data)
+    return "done"
+
